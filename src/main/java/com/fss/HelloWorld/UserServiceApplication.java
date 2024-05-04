9aaf6e825111875f77e79465ee9a2594918957a9
Kailas Elamkar <kailaselamkar@yahoo.co.in>
2024-05-04 21:01:33 +0530

    Initial Commit

---
+++ src/main/java/com/fss/HelloWorld/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.fss.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test application.");
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fss/HelloWorld/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fss.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
