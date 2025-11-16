5e9fa14f276bf596636fbaed7b08d9f6b3f055be
Kailas Elamkar <kailaselamkar@yahoo.co.in>
2025-11-16 21:34:29 +0530

    fffffweeeee

--- src/main/java/com/fss/HelloWorld/UserServiceApplication.java
+++ src/main/java/com/fss/HelloWorld/UserServiceApplication.java
@@ -9,7 +9,7 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		System.out.println("This is a test application.Hello World");
 		SpringApplication.run(UserServiceApplication.class, args);
-		System.out.println(" Added test for N8N  ");
+		System.out.println(" Added test for N8N integration ");
 
 	}
 
