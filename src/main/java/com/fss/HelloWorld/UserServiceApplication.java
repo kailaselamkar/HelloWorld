94489e798a9250fb746573cf46580ff860f27038
Kailas Elamkar <kailaselamkar@yahoo.co.in>
2025-11-16 21:27:20 +0530

    Test for N8N Trigger

--- src/main/java/com/fss/HelloWorld/UserServiceApplication.java
+++ src/main/java/com/fss/HelloWorld/UserServiceApplication.java
@@ -9,6 +9,8 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		System.out.println("This is a test application.Hello World");
 		SpringApplication.run(UserServiceApplication.class, args);
+		System.out.println("Added test for N8N");
+
 	}
 
 }
